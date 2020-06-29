8331c4b1d25c5b0adab090b9b415a7bb3c72a11f
Maciej Siara <siaramaciek1@gmail.com>
2020-06-29 14:06:54 +0200

    Updates

---
+++ precache-manifest.824c2fdf2a9aba85314b5e692e15f7dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b7361f8e5a779cfaf09fc58688f2842",
+    "url": "/Simple-Quiz/index.html"
+  },
+  {
+    "revision": "72f74b0f42e35fdb99a3",
+    "url": "/Simple-Quiz/static/css/main.5cac781e.chunk.css"
+  },
+  {
+    "revision": "50559d336cb60f99c7ca",
+    "url": "/Simple-Quiz/static/js/2.937432f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Simple-Quiz/static/js/2.937432f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72f74b0f42e35fdb99a3",
+    "url": "/Simple-Quiz/static/js/main.d96204bf.chunk.js"
+  },
+  {
+    "revision": "d204ba7adc0f21c10944",
+    "url": "/Simple-Quiz/static/js/runtime-main.a82b976e.js"
+  }
+]);
